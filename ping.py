2ef6ed710492b03cf2ce4ae982eca44785f523e5
Dare78 <Dare78@users.noreply.github.com>
2016-05-02 15:23:06 -0700

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+def check_ping():
+	for ip in range(1,256):
+		hostname = "192.168.1."+ str(ip)
+	#hostname = "192.168.1.1"
+    
+        response = os.system("ping -c 1 " + hostname)
+    # and then check the response...
+        if response == 0:
+        	pingstatus = "Network Active"
+    	else:
+        	pingstatus = "Network Error"
+
+	#print(response, pingstatus)
