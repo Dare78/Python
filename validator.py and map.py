1a582700f77a5859fe0595f55ebab08300c669bf
Dare78 <Dare78@users.noreply.github.com>
2016-05-03 00:32:13 -0700

    Update validator.py and map.py

--- validator.py and map.py
+++ validator.py and map.py
@@ -8,3 +8,14 @@ print any(c.isupper() for c in s)
 
 #map
 print(map(lambda x: x**2, [1,2,3]))
+
+#use of split and join
+
+str = ("hi how are you")
+y = str.split(" ")
+#after spliting the string
+print (y)
+
+z = " ".join(y)
+#after joining the string
+print ("After joining the string : " + z)
