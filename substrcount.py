d623a696f0c1e858c78d030e0a75ae491c345e7a
Dare78 <Dare78@users.noreply.github.com>
2016-05-03 00:37:55 -0700

    Update substrcount.py

--- substrcount.py
+++ substrcount.py
@@ -1,6 +1,7 @@
-string, substring = [raw_input() for _ in range(2)]
-score = 0
+string = raw_input()
+substring = raw_input()
+count = 0
 for i in range(len(string)):
    if string[i:i+len(substring)] == substring:
-        score += 1
+        count += 1
 print score
